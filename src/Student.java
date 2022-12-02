9542b4438e9a5e06ea07e58479f5fd1f0c4cf56f
abhijitarade <abhijitarade6106@gmail.com>
2022-12-02 16:08:34 +0530

    Student Details first commit

---
+++ src/Student.java
@@ -0,0 +1,23 @@
+
+public class Student {
+	int rollno;
+	String name;
+	String course;
+	String email;
+	long mobile;
+	String address;
+	char gender;
+	int yearofjoining;
+	public void printStudent()
+	{
+		System.out.println("Student RollNo : "+rollno);
+		System.out.println("Student Name : "+name);
+		System.out.println("Student Course : "+course);
+		System.out.println("Student Email : "+email);
+		System.out.println("Student MobileNo : "+mobile);
+		System.out.println("Student Address : "+address);
+		System.out.println("Student Gender : "+gender);
+		System.out.println("Student yearogjoining : "+yearofjoining);
+	}
+	
+}
---
+++ src/StudentManager.java
@@ -0,0 +1,16 @@
+
+public class StudentManager {
+
+	public static void main(String[] args) {
+		Student s1=new Student();
+		s1.rollno=101;
+		s1.name="Abhijit";
+		s1.course="Java FullStack";
+		s1.mobile=7263886106l;
+		s1.address="Kolhapur";
+		s1.gender='M';
+		s1.yearofjoining=2022;
+		s1.printStudent();
+	}
+
+}
